321348496824094e0657aa3467c4474e89a4e404
xander <shuaibualexander@gmail.com>
2023-04-23 19:44:56 +0100

    include emoji and gif picker

--- apps/web/src/components/Messages/Push/MessageBody.tsx
+++ apps/web/src/components/Messages/Push/MessageBody.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import EmojiPicker from 'emoji-picker-react';
+import GifPicker from 'gif-picker-react';
+import React, { useState } from 'react';
 import { Input } from 'ui';
 
+type GIFType = {
+  url: String;
+  height: Number;
+  width: Number;
+};
+
 export default function MessageBody() {
+  const [emojiOpen, setEmojiOpen] = useState(false);
+  const [gifOpen, setGifOpen] = useState(false);
+  const [inputText, setInputText] = useState('');
+
+  const appendEmoji = ({ emoji }: { emoji: string }) => setInputText(`${inputText}${emoji}`);
+  const appendGIF = (emojiObject: GIFType) => {
+    console.log({ emojiObject });
+  };
+  const submitText = () => {
+    console.log({ inputText });
+  };
+
+  const gifSample = {
+    url: 'https://media.tenor.com/YGNEnwUYCf4AAAAC/annoyed-irritated.gif'
+  };
+
   return (
     <section className="h-full	p-5 pb-3">
       <div className="h-[85%] max-h-[85%] overflow-scroll">
@@ -20,21 +44,67 @@ export default function MessageBody() {
                 </p>
                 <span className="absolute bottom-1.5	right-1.5 text-xs text-gray-500">8:25</span>
               </div>
+              <div className="relative w-fit rounded-xl rounded-tl-sm border">
+                <img
+                  className="font-medium0 relative w-fit rounded-xl rounded-tl-sm border"
+                  src={gifSample.url}
+                  alt=""
+                />
+                <img className="absolute right-2.5 top-2.5" src="/push/giticon.svg" alt="" />
+              </div>
               <div className="relative w-fit	max-w-[80%] self-end rounded-xl rounded-tr-sm border bg-violet-500 py-3 pl-4 pr-9 font-medium">
                 <p className="text-sm	text-white">Group chats, video calls... I wonder what’s next</p>
                 <span className="absolute bottom-1.5	right-1.5 text-xs text-white">8:25</span>
+              </div>
+              <div className="relative self-end overflow-hidden rounded-xl rounded-tr-sm border bg-violet-500">
+                <img
+                  className={`relative w-fit self-end rounded-xl rounded-tr-sm border bg-violet-500`}
+                  src={gifSample.url}
+                  alt=""
+                />
+                <img className="absolute right-2.5 top-2.5" src="/push/giticon.svg" alt="" />
               </div>
             </div>
           </section>
         ))}
       </div>
       <div className="relative mt-2">
-        <img className="absolute left-2 top-2.5 cursor-pointer" src="/push/emoji.svg" alt="" />
+        <img
+          onClick={() => setEmojiOpen((o) => !o)}
+          className="absolute left-2 top-2.5 cursor-pointer"
+          src="/push/emoji.svg"
+          alt=""
+        />
         <div className="absolute right-4 top-2 flex items-center gap-5">
-          <img className="relative cursor-pointer" src="/push/gif.svg" alt="gif" />
-          <img className="relative cursor-pointer" src="/push/send.svg" alt="send" />
+          <img
+            onClick={() => setGifOpen((o) => !o)}
+            className="relative cursor-pointer"
+            src="/push/gif.svg"
+            alt="gif"
+          />
+          <img onClick={submitText} className="relative cursor-pointer" src="/push/send.svg" alt="send" />
         </div>
-        <Input className="pl-11" type="text" placeholder="Type your message..." />
+        {emojiOpen ? (
+          <div className="absolute bottom-[50px]">
+            <EmojiPicker onEmojiClick={appendEmoji} />
+          </div>
+        ) : (
+          ''
+        )}
+        {gifOpen ? (
+          <div className="absolute bottom-[50px] right-0">
+            <GifPicker onGifClick={appendGIF} tenorApiKey={String(process.env.NEXT_PUBLIC_GOOGLE_TOKEN)} />
+          </div>
+        ) : (
+          ''
+        )}
+        <Input
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+          className="pl-11"
+          type="text"
+          placeholder="Type your message..."
+        />
       </div>
     </section>
   );
