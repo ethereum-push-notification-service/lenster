947d9da6945b91830c355ab72eff9f1a07d2ca6f
Monalisha Mishra <mishramonalisha76@gmail.com>
2023-05-03 18:59:43 +0530

    added date in chat

--- apps/web/src/components/Messages/Push/helper.ts
+++ apps/web/src/components/Messages/Push/helper.ts
@@ -1,5 +1,6 @@
 import type { IUser } from '@pushprotocol/restapi';
 import { LENSHUB_PROXY } from 'data';
+import moment from 'moment';
 import { CHAIN_ID } from 'src/constants';
 
 export const getProfileFromDID = (did: string) => {
@@ -15,4 +16,21 @@ export const isProfileExist = (connectedProfile: IUser | undefined) => {
     return false;
   }
   return true;
+};
+
+export const dateToFromNowDaily = (timestamp: number): string => {
+  var fromNow = moment(timestamp).fromNow();
+  const timestampDate = moment(timestamp).calendar(null, {
+    // when the date is closer, specify custom values
+    lastWeek: '[Last] dddd',
+    lastDay: '[Yesterday]',
+    sameDay: '[Today]',
+    // nextDay:  '[Tomorrow]',
+    nextWeek: 'dddd',
+    // when the date is further away, use from-now functionality
+    sameElse: function () {
+      return '[' + fromNow + ']';
+    }
+  });
+  return timestampDate;
 };
