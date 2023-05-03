90f7ca1319fc096b2cb5e3b5029116494c489721
Monalisha Mishra <mishramonalisha76@gmail.com>
2023-05-03 19:02:53 +0530

    added date in chatbox

--- apps/web/src/components/Messages/Push/helper.ts
+++ apps/web/src/components/Messages/Push/helper.ts
@@ -19,18 +19,12 @@ export const isProfileExist = (connectedProfile: IUser | undefined) => {
 };
 
 export const dateToFromNowDaily = (timestamp: number): string => {
-  var fromNow = moment(timestamp).fromNow();
   const timestampDate = moment(timestamp).calendar(null, {
-    // when the date is closer, specify custom values
     lastWeek: '[Last] dddd',
     lastDay: '[Yesterday]',
     sameDay: '[Today]',
-    // nextDay:  '[Tomorrow]',
     nextWeek: 'dddd',
-    // when the date is further away, use from-now functionality
-    sameElse: function () {
-      return '[' + fromNow + ']';
-    }
+    sameElse: 'DD/MM/YYYY'
   });
   return timestampDate;
 };
