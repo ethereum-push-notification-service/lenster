8cdd67943c4b9627730dc0a1eb95baa2959f3130
KlausMikhaelson <satyamsingh5076@gmail.com>
2023-05-20 19:56:33 +0530

    added dark mode in ongoing call

--- apps/web/src/components/Messages/Push/Video/OngoingCall.tsx
+++ apps/web/src/components/Messages/Push/Video/OngoingCall.tsx
@@ -6,9 +6,10 @@ const OngoingCall = () => {
     return (
         <div className='pt-4 pb-4'>
             <span className="mb-2 sm:flex md:flex hidden absolute left-0 right-0 top-5 m-auto flex items-center justify-center sm:static md:static">
-                <div className="mb-2 flex items-center rounded-lg bg-[#F4F4F5] px-2 py-0.5 sm:p-2 md:p-2">
-                    <Image className="mr-2 h-2 " src="/push/lock.svg" alt="lock" />
-                    <span className="text-[10px] text-[#9E9EA9] sm:text-[13px] md:text-[15px]">
+                <div className="mb-2 flex items-center rounded-lg bg-[#F4F4F5] dark:bg-[#18181B] px-2 py-0.5 sm:p-2 md:p-2">
+                    <Image className="dark:hidden mr-2 h-4" src="/push/lock.svg" alt="lock" />
+                    <Image className="dark:flex hidden mr-2 h-4" src="/push/lockdark.svg" alt="lock" />
+                    <span className="text-[10px] text-[#9E9EA9] dark:text-[#D4D4D8] sm:text-[13px] md:text-[15px]">
                         End-to-end encrypted
                     </span>
                 </div>
