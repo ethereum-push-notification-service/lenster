de787234a0a2011cb9f6c3b8ec83b358ab8c315f
Madhur Gupta <madhur@push.org>
2023-05-25 19:05:13 +0530

    Remove e2e encrypted UI

--- apps/web/src/components/Messages/Push/Video/OngoingCall.tsx
+++ apps/web/src/components/Messages/Push/Video/OngoingCall.tsx
@@ -5,7 +5,7 @@ import Video from '@components/Messages/Push/Video/Video';
 import usePushVideoCall from '@components/utils/hooks/push/usePushVideoCall';
 import React from 'react';
 import { usePushChatStore } from 'src/store/push-chat';
-import { Card, Image } from 'ui';
+import { Card } from 'ui';
 
 import { getProfileFromDID } from '../helper';
 
@@ -28,23 +28,6 @@ const OngoingCall = () => {
       <div className="flex w-[100%] max-w-[100%] grow items-center justify-center  sm:w-[100%] md:w-[100%] 2xl:max-w-screen-xl">
         <Card className="mt-1 w-[100%] sm:mt-4 sm:w-[90%] md:mt-4 md:w-[95%]">
           <div className="pb-4 pt-4">
-            <span className="absolute left-0 right-0 top-5 m-auto mb-2 flex hidden items-center justify-center sm:static sm:flex md:static md:flex">
-              <div className="mb-2 flex items-center rounded-lg bg-[#F4F4F5] px-2 py-0.5 dark:bg-[#18181B] sm:p-2 md:p-2">
-                <Image
-                  className="mr-2 h-4 dark:hidden"
-                  src="/push/lock.svg"
-                  alt="lock"
-                />
-                <Image
-                  className="mr-2 hidden h-4 dark:flex"
-                  src="/push/lockdark.svg"
-                  alt="lock"
-                />
-                <span className="text-[10px] text-[#9E9EA9] dark:text-[#D4D4D8] sm:text-[13px] md:text-[15px]">
-                  End-to-end encrypted
-                </span>
-              </div>
-            </span>
             <div className="relative">
               <Video
                 isVideoOn={isIncomingVideoOn}
--- apps/web/src/components/Messages/Push/Video/OutgoingCallModal.tsx
+++ apps/web/src/components/Messages/Push/Video/OutgoingCallModal.tsx
@@ -7,7 +7,7 @@ import usePushVideoCall from '@components/utils/hooks/push/usePushVideoCall';
 import { VideoCallStatus } from '@pushprotocol/restapi';
 import { useEffect } from 'react';
 import { usePushChatStore } from 'src/store/push-chat';
-import { Image, Modal } from 'ui';
+import { Modal } from 'ui';
 
 const OutgoingCallModal = () => {
   const connectedProfile = usePushChatStore((state) => state.connectedProfile);
@@ -45,23 +45,6 @@ const OutgoingCallModal = () => {
   return (
     <Modal size="md" show={isModalVisible}>
       <div className="m-0 my-0 sm:my-4 md:my-4">
-        <span className="absolute left-0 right-0 top-8 m-auto flex items-center justify-center sm:static md:static">
-          <div className="mb-2 flex items-center rounded-lg bg-[#F4F4F5] px-2 py-0.5 dark:bg-[#18181B] sm:p-2 md:p-2">
-            <Image
-              className="mr-2 flex h-4 dark:hidden"
-              src="/push/lock.svg"
-              alt="lock"
-            />
-            <Image
-              className="mr-2 hidden h-4 dark:flex"
-              src="/push/lockdark.svg"
-              alt="lock"
-            />
-            <span className="text-[10px] text-[#9E9EA9] dark:text-white sm:text-[13px] md:text-[15px]">
-              End-to-end encrypted
-            </span>
-          </div>
-        </span>
         <div className="absolute left-0 right-0 top-16 z-50 m-auto mt-2 flex items-center justify-center sm:static sm:flex md:static md:flex">
           <ProfileInfo profileId={getProfileFromDID(selectedChatId)} />
         </div>
